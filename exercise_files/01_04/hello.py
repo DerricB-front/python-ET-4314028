29b62c7f61bd146a5e39960d4855cab3a561b826
Derric-resultay <derricbresultay@gmail.com>
2025-08-23 08:19:05 +0800

    added some basic comments

--- exercise_files/01_04/hello.py
+++ exercise_files/01_04/hello.py
@@ -1,3 +1,14 @@
 
 # Print "Hello, World!" to the terminal
 print('Hello, World!')
+
+# adding two int
+print(1 + 1)
+
+# putting a value into a variable
+x = "Derric will change"
+print(x)
+
+# check the type of x, str
+type(x)
+
